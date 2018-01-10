8f630a409de2d18e6c3f71c65451e9350a63c83c
zakgit <30319255+zakgit@users.noreply.github.com>
2018-01-10 23:32:22 +0530

    initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styles: [`
+    h3{
+      color: dodgerblue;
+    }
+  `]
+})
+export class AppComponent {
+  name = '';
+}
---
+++ src/app/shopping-list/shopping-list.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+import { Ingredient } from '../shared/ingredients.model';
+
+@Component({
+  selector: 'app-shopping-list',
+  templateUrl: './shopping-list.component.html',
+  styleUrls: ['./shopping-list.component.css']
+})
+export class ShoppingListComponent implements OnInit {
+  ingredients: Ingredient[] = [
+    new Ingredient('Apples', 100),
+    new Ingredient('Tomato', 10)
+  ];
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
